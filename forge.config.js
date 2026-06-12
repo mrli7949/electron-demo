f660e612e0d87bec8caf47c005d940a61d807583
sg <458034282@qq.com>
2026-06-12 15:47:30 +0800

    feat: 改一下根依赖

--- forge.config.js
+++ forge.config.js
@@ -1,5 +1,5 @@
-const { FusesPlugin } = require('@electron-forge/plugin-fuses');
-const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+const { FusesPlugin } = require("@electron-forge/plugin-fuses");
+const { FuseV1Options, FuseVersion } = require("@electron/fuses");
 
 module.exports = {
   packagerConfig: {
@@ -8,25 +8,42 @@ module.exports = {
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
+      name: "@electron-forge/maker-squirrel",
       config: {},
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
     },
     {
-      name: '@electron-forge/maker-deb',
-      config: {},
+      name: "@electron-forge/maker-deb",
+      config: {
+        options: {
+          maintainer: "your name",
+          homepage: "https://example.com",
+          categories: ["Utility"],
+          depends: [
+            "libgtk-3-0t64 | libgtk-3-0",
+            "libnotify4t64 | libnotify4",
+            "libnss3",
+            "xdg-utils",
+            "libatspi2.0-0t64 | libatspi2.0-0",
+            "libdrm2",
+            "libgbm1",
+            "libxcb-dri3-0",
+            "trash-cli | libglib2.0-bin",
+          ],
+        },
+      },
     },
     {
-      name: '@electron-forge/maker-rpm',
+      name: "@electron-forge/maker-rpm",
       config: {},
     },
   ],
   plugins: [
     {
-      name: '@electron-forge/plugin-auto-unpack-natives',
+      name: "@electron-forge/plugin-auto-unpack-natives",
       config: {},
     },
     // Fuses are used to enable/disable various Electron functionality
